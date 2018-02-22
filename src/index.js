3d31926aaf4823c27ce9e0e69b89e18917886e83
Artyom <artloer98@gmail.com>
2018-02-23 01:45:05 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var roots = [], arr = equation.split(' ');
+  var a = +arr[0], b = +(arr[3] + arr[4]), c = +(arr[7] + arr[8]);
+  var discriminant = Math.pow(b, 2) - (4 * a * c);
+  var x1 = (-b + Math.sqrt(discriminant)) / (2 * a), x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+  x1 = +x1.toFixed(1);
+  x2 = +x2.toFixed(1);
+  if(x1 < x2) {
+    roots.push(x1);
+    roots.push(x2);
+  } else {
+    roots.push(x2);
+    roots.push(x1);
+  };
+  return roots;
 }
